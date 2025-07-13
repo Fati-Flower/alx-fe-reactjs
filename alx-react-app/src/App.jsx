e54eb7cdc1f1826506c2fa82eb38c203ed96f173
Mac <samed-mac@192.168.1.16>
2025-07-13 15:56:15 +0100

    updat 2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,11 +8,22 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 
+import './App.css';
+import UserProfile from './components/UserProfile';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
+
+    
     <div>
+  <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+
       <Header />
       <WelcomeMessage />
       <MainContent />
